e122ba5a88df88ec83b75f05023ea4790050afe8
Ramanchoudhary07 <ramantarar1@gmail.com>
2024-10-22 18:14:03 +0530

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
-import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import projectRoutes from "./routes/project.routes.js";
@@ -14,19 +14,24 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// app.use(`/`, (req, res) => {
+//   if (process.env) {
+//     res.send({ ENV: process.env });
+//   } else {
+//     res.send("No .env file");
+//   }
+// });
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(`/api/auth`, authRoutes);
 app.use(`/api/project`, projectRoutes);
 app.use(`/api/contact`, contactRoutes);
-
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, `/frontend/dist`)));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   connectDB();
